a0eb84a552eafedeb1996f99e0eb1d2d2797e70a
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-05-20 23:58:38 +0200

    Completed CRUD Exercise

---
+++ scripts/addUser.php
@@ -0,0 +1,20 @@
+<?php
+// Testing
+
+include 'db_connection.php';
+
+if(empty($_POST['name'] && $_POST['age'] && $_POST['occupation'])){
+    echo "Hey, you need to enter in all the data please!";
+} else {
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $occupation = $_POST['occupation'];
+
+    $sql = "INSERT INTO users (id, name, age, occupation) VALUES (NULL,'$name','$age','$occupation');";
+    $result = mysqli_query($conn, $sql);
+    header("Location: http://localhost:8888/PhpFrontAndBack");
+}
+
+
+
+?>
---
+++ scripts/db_connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpassword = "root";
+$dbname = "testing";
+
+$conn = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
+
+?>
